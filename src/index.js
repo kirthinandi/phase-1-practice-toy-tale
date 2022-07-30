0361e3cd9f165a544ea62cd272ed47fa93091921
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-30 14:31:51 -0700

    commit tree 75e5a6f530412277ad85f4e0d6179e1aeb97c09b

--- src/index.js
+++ src/index.js
@@ -1,8 +1,77 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.querySelector("#toy-collection");
+
+  const displayCard = (toy) => {
+    const toyCard = document.createElement("div");
+    toyCard.className = "card";
+    const h2 = document.createElement("h2");
+    h2.textContent = toy.name;
+    toyCard.append(h2);
+    const img = document.createElement("img");
+    img.className = "toy-avatar";
+    img.src = toy.image;
+    toyCard.append(img);
+    const p = document.createElement("p");
+    p.innerText = `${toy.likes} Likes`;
+    toyCard.append(p);
+    const btn = document.createElement("btn");
+
+    btn.addEventListener("click", () => {
+      toy.likes += 1;
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ likes: toy.likes }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          p.innerText = `${toy.likes} Likes`;
+        });
+    });
+
+    btn.className = "like-btn";
+    btn.id = toy.id;
+    btn.innerText = "Like ❤️";
+    toyCard.append(btn);
+    toyCollection.append(toyCard);
+  };
+
+  fetch("http://localhost:3000/toys")
+    .then((r) => r.json())
+    .then((data) => {
+      data.forEach(displayCard);
+    });
+
+  const toyForm = document.querySelector(".add-toy-form");
+
+  toyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newToy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newToy),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        displayCard(data);
+        e.target.reset();
+      });
+  });
+
   const addBtn = document.querySelector("#new-toy-btn");
+
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
